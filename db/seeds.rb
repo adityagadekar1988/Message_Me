9f3000f14e70bf646374b469c2761ebe851d8aaa
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-21 16:48:37 +0530

    Added users using seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "adityagadekar", password: "123456")
+User.create(username: "adigadekar", password: "123456")
+User.create(username: "adgadekar", password: "123456")
