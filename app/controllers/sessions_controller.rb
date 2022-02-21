645fae7e6bc06b1451860d9f0b044211c099ae23
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-21 18:47:49 +0530

    Add sessions create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
   def new
+  end 
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password_digest])
+      session[:user_id] = user.id 
+      flash[:success] = "You are successfully logged in"
+      redirect_to root_path
+    else 
+      flash.now[:error] = "Sorry!!Wrong Login Credentials"
+      render 'new'
+    end 
+  end 
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You are successfully logged out"
+    redirect_to login_path
   end 
 end
